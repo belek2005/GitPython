e4223e250adae2658ca9133192c361bfcb5077da
belek2005 <81576474+belek2005@users.noreply.github.com>
2021-04-12 14:49:17 +0600

    Add files via upload

---
+++ homework7.py
@@ -0,0 +1,15 @@
+file = [str(10) + str(10) for l in range(100)]
+
+f = open('test.txt', 'w')
+
+for l in file:
+	f.write("loremIpsum" + '\n')
+
+f.close()
+
+f = open('test.txt', 'r')
+
+print(f.read(10))
+print(f.read(12))
+print(f.read(11))
+print(f.read(10))
