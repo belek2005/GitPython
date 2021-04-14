4138edee9f7756b8f936e82cdf966aec1cad0aa5
belek2005 <81576474+belek2005@users.noreply.github.com>
2021-04-14 21:24:22 +0600

    Add files via upload

---
+++ lesson9_1.py
@@ -0,0 +1,42 @@
+# import datetime
+import time
+
+print(time.ctime(time.time()))
+# print(time.time())
+# print(time.strftime("%d/%m/%Y %H/%M/%S", time.localtime()))
+# print(time.localtime())
+# for x in range(1, 60):
+# 	print(x)
+# 	time.sleep(1)
+# print(time.ctime(13000))
+# print (time.gmtime(0))
+# date = datetime.date(2021, 5, 14)
+
+# print(date.year)
+# print(date.month)
+# print(date.day)
+
+# print(datetime.date.today())
+
+# d = datetime.datetime(2021, 4, 14, 3, 50 ,4)
+# print(d.year)
+# print(d.hour)
+# print(d.second)
+
+# print(datetime.datetime.today().strftime("%d/%m/%y %H:%M:%S"))
+
+# now = datetime.datetime.now()
+# then = datetime.datetime(2021, 3, 14)
+
+# delta = now - then
+
+# print(delta.days)
+# print(delta.seconds)
+
+# seconds = delta.total_seconds()
+
+# hours = seconds // 3600
+# minutes = (seconds % 3600) // 60
+
+# print(hours)
+# print(minutes)
