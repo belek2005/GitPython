30656aa9bc71b6933c0a4c0ef9ebe47b2ca51ac1
belek2005 <81576474+belek2005@users.noreply.github.com>
2021-04-14 19:05:04 +0600

    Add files via upload

---
+++ homework8.py
@@ -0,0 +1,25 @@
+import csv
+
+def csv_writer(data, path):
+	with open(path, "w") as csv_file:
+		writer = csv.writer(csv_file, delimiter=",")
+		for line in data:
+			writer.writerow(line)
+
+data = [
+	"username,email,ip_address".split(","),	
+	"root,root@example.com,192.168.0.2".split(","),
+	"admin,root@example.com,192.168.0.2".split(","),
+	"test_user,root@example.com,192.168.0.2".split(","),
+	"second_user,root@example.com,192.168.0.2".split(",")
+]
+
+path = "data.csv"
+
+csv_writer(data, path)
+
+def csv_reader(path, file_obj):
+	reader = csv.Reader(path,file_obj)
+
+with open("data.csv", "r") as file_obj:
+	csv_reader(file_obj)
